b8b7e6ab9c0d741b68210add53dbc12640c96889
alihm961 <alihasanmazloum@gmail.com>
2025-06-27 17:42:04 +0200

    Snacks Added

---
+++ models/SnackOrder.php
@@ -0,0 +1,11 @@
+<?php
+require_once('Model.php');
+
+class SnackOrder extends Model {
+    public function create($booking_id, $snack_name, $quantity, $price) {
+        return $this->execute("
+        INSERT INTO snack_orders (booking_id, snack_name, quantity, price) VALUES (?, ?, ?, ?)",
+        [$booking_id, $snack_name, $quantity, $price], "isid"
+     );
+    }
+}
